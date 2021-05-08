fd1e4e86a9a2a020905a6eb1c0dbf582efbb7d83
artemis-2701 <61738043+artemis-2701@users.noreply.github.com>
2021-05-08 11:57:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def about():
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 @app.route('/contact/')
 def contact():
     return render_template('contact.html')
